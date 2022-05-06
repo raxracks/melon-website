06cede3d74a65e0f32cf40d18c7a62f12671a32a
raxracks <multisniperism@gmail.com>
2022-05-06 22:28:02 +1200

    first commit

---
+++ components/HelloWorld.tsx
@@ -0,0 +1,38 @@
+import Tab from "./Tab";
+import styles from "../styles/Home.module.css";
+
+export default function HelloWorld() {
+  return (
+    <code className={styles.code}>
+      &#123;
+      <br />
+      <Tab />
+      <span className="green">// This Code prints "Hello World" in Melon Language</span>
+      <br />
+      <br />
+      <Tab />
+      <span className="purple">importl</span>
+      <br />
+      <Tab />
+      graphics
+      <br />
+      <br />
+      <Tab />
+      <span className="blue">string</span> helloWorld ={" "}
+      <span className="orange">"Hello%0aWorld!"</span> ? Locked ={" "}
+      <span className="lblue">
+        false
+        <Tab />
+      </span>
+      <br />
+      <br />
+      <Tab />
+      <span className="purple">println</span>
+      <br />
+      <Tab />
+      &#123;helloWorld&#125;
+      <br />
+      &#125;
+    </code>
+  );
+}
